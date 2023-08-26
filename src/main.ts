5f2092c01d960a2ea29e83b5836f0119ee3bf5bb
Ivan <vanfretes@gmail.com>
2023-08-26 18:54:04 -0400

    feat: plugin vue for build

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import TreeMenu from './components/TreeMenu/TreeMenu.vue'
+import TreeNode from './components/TreeMenu/TreeNode.vue'
+import ToggleIcon from './components/TreeMenu/ToggleIcon.vue'
+
+
+export {TreeMenu, TreeNode, ToggleIcon}
+
+//import App from './App.vue'
+// createApp(App).mount('#app')
